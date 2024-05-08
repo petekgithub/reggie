3573af6b73173a11d6227154518848580544445e
Petech <petekshamitbeyli@gmail.com>
2024-05-08 22:08:21 +0300

    project-setup-and-chatbot-logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [messages, setMessages] = useState([
+    {
+      content: "It's over greenhead.",
+      role: "assistant",
+    },
+    {
+      content: "You underestimate my power!",
+      role: "user",
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="container mx-auto p-5 inset-0">
+      <div className="mockup-window border bg-base-300 w-full h-full flex flex-col">
+        <div className="p-5 pb-8 flex-grow overflow-auto">
+          {messages.length &&
+            messages.map((msg, index) => {
+              return (
+                <div
+                  className={`chat ${
+                    msg.role === "assistant" ? "chat-start" : "chat-end"
+                  }`}
+                  key={index}
+                >
+                  <div className="chat-image avatar">
+                    <div className="w-10 rounded-full">
+                      <img
+                        alt="avatars"
+                        src={
+                          msg.role === "assistant"
+                            ? "/images/assistant.png"
+                            : "/images/user.png"
+                        }
+                      />
+                    </div>
+                  </div>
+                  <div className="chat-bubble">{msg.content}</div>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </section>
   );
 }
 
